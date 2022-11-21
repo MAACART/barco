7bfb48815fe51ee880c6c912a2eba97cb2900018
MAACART <66652570+MAACART@users.noreply.github.com>
2022-11-21 10:37:27 -0300

    novo mar - Vinicius

--- sketch_221031d.js
+++ sketch_221031d.js
@@ -130,6 +130,41 @@ function draw() {
     //ativa a obra
     Sereia();
      break;
+      
+      /NOVO - COMEÇO
+  translate(0,110);
+  fill(255,255,255, 0);
+ if (mouseButton == LEFT) {
+    fill(70,130,180, 1000 /*trocar esse valor para transparência*/);
+  }
+  // We are going to draw a polygon out of the wave points
+  beginShape();
+
+  let xoff = 0; // Option #1: 2D Noise
+  // let xoff = yoff; // Option #2: 1D Noise
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0, 1, 200, 300);
+
+    // Option #2: 1D Noise
+    // let y = map(noise(xoff), 0, 1, 200,300);
+
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.06;
+  }
+  // increment y dimension for noise
+  yoff += 0.0005;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+  
+//NOVO - FIM
 
    }
 
